bf374f347381e9bcd7401e5ca91d5898c1bc72ea
contentkingpins <asiegel@contentkingpins.com>
2025-03-14 21:35:20 -0400

    Update phone number to +1 833-998-6906 and add responsive call-to-action text

--- src/components/claim/ClaimForm.tsx
+++ src/components/claim/ClaimForm.tsx
@@ -599,13 +599,14 @@ export default function ClaimForm() {
                 </p>
                 
                 <a
-                  href="tel:8885555555" 
+                  href="tel:+18339986906" 
                   className="btn-primary text-lg py-4 px-8 inline-flex items-center"
                 >
                   <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                   </svg>
-                  Click to Call
+                  <span className="hidden md:inline">Call +1 (833) 998-6906</span>
+                  <span className="md:hidden">Click to Call</span>
                 </a>
                 
                 <p className="text-sm text-gray-500 mt-6">
@@ -706,14 +707,15 @@ export default function ClaimForm() {
         {isRejected && (
           <div className="flex justify-center mt-8">
             <a
-              href="tel:8885555555"
+              href="tel:+18339986906"
               className="btn-primary"
             >
               <div className="flex items-center justify-center">
                 <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                   <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                 </svg>
-                Please Click to Call a Case Manager Now
+                <span className="hidden md:inline">Call +1 (833) 998-6906</span>
+                <span className="md:hidden">Please Click to Call a Case Manager Now</span>
               </div>
             </a>
           </div>
